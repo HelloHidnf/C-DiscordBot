7eb26f81d19ec5f0a5cd63090e97a58dcc33b4ac
HelloHidnf <HelloHidnf@gmail.com>
2023-06-10 16:35:12 +0100

    Add project files.

---
+++ Modules/Commands.cs
@@ -0,0 +1,163 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using EightBallApiWrapper;
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+        Color custom = new Color(0x00ffc8);
+
+        //macros
+        [Command("macros")]
+        public async Task macros()
+        {
+            var icon = Context.Guild.IconUrl;
+
+            var embed = new EmbedBuilder
+            {
+                Title = "Macros",
+                Description = "creator\nvote\n8ball\ncry",
+                Color = custom
+            };
+            embed.AddField("Admin Macros",
+                "say\npurge\nupurge")
+                .WithFooter("Current preset is .")
+                .WithThumbnailUrl(icon);
+
+            try
+            {
+                var dm = await Context.Message.Author.CreateDMChannelAsync();
+                await dm.SendMessageAsync(embed: embed.Build());
+            }
+            catch
+            {
+                var m = await Context.Message.ReplyAsync(embed: embed.Build());
+                await Task.Delay(10000);
+                await m.DeleteAsync();
+            }
+            
+            await Context.Message.DeleteAsync();
+        }
+
+        //creator
+        [Command("creator")]
+        public async Task creator()
+        {
+            await Context.Message.DeleteAsync();
+
+            var embed = new EmbedBuilder
+            {
+                Title = "HelloHidnf",
+                Description = "Hello, the creator of this bot is HelloHidnf, links for youtube and twitch are down below, don't expect many uploads"
+            };
+
+            embed.AddField("Socials",
+                "[Youtube](https://www.youtube.com/channel/UChMrrmwVdde88CIV4ZH0_rw)\n[Twitch](https://www.twitch.tv/hellohidnf1)")
+                .WithAuthor(Context.Client.GetUser(410643436044156938))
+                .WithColor(custom);
+
+            await ReplyAsync(embed: embed.Build());
+        }
+
+        //Voting command
+        [Command("vote")]
+        public async Task test([Remainder] string question)
+        {
+            Emoji check = new Emoji("✅");
+            Emoji cross = new Emoji("❌");
+
+            var embed = new EmbedBuilder
+            {
+                Title = question,
+                Color = custom
+            }.WithAuthor(Context.User).Build();
+
+            var vote = await Context.Channel.SendMessageAsync(embed: embed);
+            await vote.AddReactionAsync(check);
+            await vote.AddReactionAsync(cross);
+            await Context.Message.DeleteAsync();
+        }
+
+        //8ball
+        [Command("8ball")]
+        public async Task _8ball([Remainder] string question)
+        {
+            EightBall ball = new EightBall();
+            var result = ball.AskQuestion(question);
+
+            var embed = new EmbedBuilder
+            {
+                Title = question,
+                Description = result.Answer,
+                Color = custom
+            }
+            .WithAuthor(Context.User).Build();
+
+            await ReplyAsync(embed: embed);
+
+            await Context.Message.DeleteAsync();
+        }
+
+        //cry
+        [Command("cry")]
+        public async Task cry()
+        {
+            Emoji crying = new Emoji("😭");
+
+            await Context.Message.AddReactionAsync(crying);
+
+            await ReplyAsync($"{Context.Message.Author.Mention} made me cry.");
+        }
+
+        //say
+        [Command("say")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task say([Remainder] string say)
+        {
+            await Context.Message.DeleteAsync();
+            await ReplyAsync(say);
+        }
+
+        //purge
+        [Command("purge")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task purge(int amount)
+        {
+            await Context.Message.DeleteAsync();
+
+            var messages = await Context.Channel.GetMessagesAsync(Context.Message, Direction.Before, amount).FlattenAsync();
+
+            var filteredMessages = messages.Where(x => (DateTimeOffset.UtcNow - x.Timestamp).TotalDays <= 14);
+
+            await (Context.Channel as ITextChannel).DeleteMessagesAsync(filteredMessages);
+        }
+
+        //user purge
+        [Command("upurge")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task purge_user(int amount, SocketUser user)
+        {
+            await Context.Message.DeleteAsync();
+
+            var messages = await Context.Channel.GetMessagesAsync(Context.Message, Direction.Before, amount).FlattenAsync();
+
+            var filteredMessages = messages.Where(x => (DateTimeOffset.UtcNow - x.Timestamp).TotalDays <= 14);
+
+            filteredMessages = messages.Where(x => (x.Author.Id == user.Id));
+
+            await (Context.Channel as ITextChannel).DeleteMessagesAsync(filteredMessages);
+        }
+
+        //sets playing ...
+        [Command("game")]
+        [RequireOwner]
+        public async Task game([Remainder] string game)
+        {
+            await Context.Client.SetGameAsync(name: game, type: ActivityType.Playing);
+            await Context.Message.DeleteAsync();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        private DiscordSocketConfig _config;
+
+        //startup
+        public async Task RunBotAsync()
+        {
+            _config = new DiscordSocketConfig() { MessageCacheSize = 100 };
+            _client = new DiscordSocketClient(_config);
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "";
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        //useless afaik but if I remove the code breaks
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        //commands ig
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageDeleted += DeLog;
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        //message recieved
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            //getting the name of the channel
+            var SChannel = message.Channel as SocketGuildChannel;
+            var type = SChannel.GetChannelType();
+
+            //filtering out logs and nsfw chats
+            if (type != ChannelType.Text) return;
+            if (SChannel.Name.ToLower() == "logs") return;
+            if (SChannel.Name.ToLower() == "nsfw") return;
+
+            //commands handler
+            int argPos = 0;
+            if (message.HasStringPrefix(".", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                //command failed
+                if (!result.IsSuccess)
+                {
+                    //basically useless, I never make errors, I would never
+                    Console.WriteLine(result.ErrorReason);
+
+                    //sending message and reaction for incorrect command
+                    var m = await message.ReplyAsync("If this was meant to be a macro, this is not a valid macro, please type .macros.");
+
+                    //countdown and deletion
+                    await Task.Delay(10000);
+
+                    await m.DeleteAsync();
+                }
+            }
+        }
+
+        //message deleted
+        private async Task DeLog(Cacheable<IMessage, ulong> Message, Cacheable<IMessageChannel, ulong> Channel)
+        {
+            Color custom = new(0x00ffc8);
+
+            //getting message info
+            var message = await Message.GetOrDownloadAsync();
+
+            //getting server and channel
+            SocketGuildChannel channel = (SocketGuildChannel)message.Channel;
+
+            var user = message.Author;
+
+            //filtering out commands + channels
+            if (message.Content.StartsWith(".")) return;
+            if (channel.Name == "logs") return;
+            if (channel.Name == "nsfw") return;
+            if (user.IsBot) return;
+
+            var des = $"<#{channel.Id}>: {message.Content}";
+
+            var server = channel.Guild;
+
+            var count = message.Attachments.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                var attachment = message.Attachments.ElementAt(i);
+                des += $"\n [{attachment}]({attachment.Url.Replace("cdn.discordapp.com", "media.discordapp.net")})";
+            }
+
+            var embed = new EmbedBuilder
+            {
+                Title = "Message deleted",
+                Description = des,
+            }
+            .WithAuthor(user)
+            .WithCurrentTimestamp()
+            .WithColor(custom);
+
+            var Log = server.TextChannels.SingleOrDefault(x => x.Name == "logs");
+
+            await Log.SendMessageAsync(embed: embed.Build());
+        }
+    }
+}
